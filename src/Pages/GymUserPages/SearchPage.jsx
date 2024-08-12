b9f6f2a9632ea32f6e3347ea40cdb25a3df4bfe8
Prashanth <prashanthbenny2008@gmail.com>
2024-08-13 02:07:26 +0530

    updated the city selection and geolocation data fetch

--- src/Pages/GymUserPages/SearchPage.jsx
+++ src/Pages/GymUserPages/SearchPage.jsx
@@ -11,17 +11,30 @@ const SearchPage = () => {
   const [cities, setCities] = useState([]);
   const [selectedCity, setSelectedCity] = useState('');
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [citiesLoaded, setCitiesLoaded] = useState(false); // New state variable
+  const [selectedPosition, setSelectedPosition] = useState(null);
   const dropdownRef = useRef(null);
 
   useEffect(() => {
     fetchCities();
-    fetchGymsInTheCity();
-    setSelectedCity('Bangalore');
   }, []);
+
+  useEffect(() => {
+    if (citiesLoaded) {
+      getCurrentLocation();
+    }
+  }, [citiesLoaded]);
+
 
   useEffect(() => {
     fetchGymsInTheCity();
   }, [selectedCity]);
+
+  useEffect(() => {
+    if(selectedPosition) {
+      setFilteredGyms(sortGymsByDistance(gyms, selectedPosition.latitude, selectedPosition.longitude));
+    }
+  }, [selectedPosition]);
 
   //to handle the dropdown close on outside click
   useEffect(() => {
@@ -47,8 +60,8 @@ const SearchPage = () => {
 
   const fetchGymsInTheCity = async (city) => {
     try {
-      const res = await axios.get(`${config.BASE_BACKEND_URL}/api/gym/search`, {
-        params: { city: selectedCity } // Hardcoded city name for now
+      const res = await axios.get(`${config.BASE_BACKEND_URL}/gyms/search`, {
+        params: { city: selectedCity.name }
       });
       setGyms(res.data);
       setFilteredGyms(res.data);
@@ -59,8 +72,9 @@ const SearchPage = () => {
 
   const fetchCities = async () => {
     try {
-      const res = await axios.get(`${config.BASE_BACKEND_URL}/api/gym/getCities`);
+      const res = await axios.get(`${config.BASE_BACKEND_URL}/cities`);
       setCities(res.data);
+      setCitiesLoaded(true);
     } catch (error) {
       console.log(error);
     }
@@ -68,9 +82,76 @@ const SearchPage = () => {
 
   const handleCityChange = (city) => {
     setSelectedCity(city);
+    setSelectedPosition({ latitude: city.latitude, longitude: city.longitude });
     setDropdownOpen(false);
     // Add any additional logic for when a city is selected
   };
+
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        findNearestCity(latitude, longitude);
+        setSelectedPosition({ latitude, longitude });
+      }, error => {
+        console.error('Error getting location:', error);
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+
+  const findNearestCity = (latitude, longitude) => {
+    const nearestCity = cities.reduce((prev, curr) => {
+      const prevDistance = haversineDistance(latitude, longitude, prev.latitude, prev.longitude);
+      const currDistance = haversineDistance(latitude, longitude, curr.latitude, curr.longitude);
+      return (prevDistance < currDistance) ? prev : curr;
+    });
+
+    setSelectedCity(nearestCity);
+  };
+
+  const haversineDistance = (lat1, lon1, lat2, lon2) => {
+    const toRad = (x) => x * Math.PI / 180;
+    const R = 6371; // Radius of the Earth in km
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+              Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  };
+
+  const sortGymsByDistance = (gyms, userLatitude, userLongitude) => {
+    return gyms.map(gym => {
+      const { latitude, longitude } = gym.map_detail;
+      const distance = haversineDistance(userLatitude, userLongitude, latitude, longitude);
+      return { ...gym, distance };
+    }).sort((a, b) => a.distance - b.distance);
+  };
+
+  const sortByNearestTime = () => {
+    // Sort gyms by nearest available time slot
+    const sortedGyms = gyms.sort((a, b) => {
+      const getEarliestTime = (slots) => {
+        const times = slots.flatMap(slot => slot.slots.map(s => s.from));
+        return new Date(`1970-01-01T${Math.min(...times.map(time => new Date(`1970-01-01T${time}:00Z`).getTime()))}Z`);
+      };
+
+      const nearestTimeA = getEarliestTime(a.availableTimeSlots);
+      const nearestTimeB = getEarliestTime(b.availableTimeSlots);
+      return nearestTimeA - nearestTimeB;
+    });
+
+    setGyms(sortedGyms);
+  };
+
+  const sortByDistance = () => setFilteredGyms(sortGymsByDistance(filteredGyms, selectedPosition.latitude, selectedPosition.longitude));
+  const sortByPrice = () => setFilteredGyms([...filteredGyms].sort((a, b) => a.price - b.price));
+  const sortByRating = () => setFilteredGyms([...filteredGyms].sort((a, b) => b.average_rating - a.average_rating));
+  const sortByTime = () => sortByNearestTime(filteredGyms);
+
 
   return (
     <div className="searchPage bg-black mx-auto px-4" style={{ minHeight: "calc(100vh - 220px)" }}>
@@ -82,7 +163,7 @@ const SearchPage = () => {
               className="bg-red-600 text-white py-2 px-4 flex justify-center items-center"
             >
               <FiMapPin className="w-5 h-5 mr-2" />
-              <span>{selectedCity}</span>
+              <span>{selectedCity.name}</span>
               <FiChevronDown className="w-5 h-5 ml-2" />
             </button>
             {dropdownOpen && (
@@ -90,7 +171,7 @@ const SearchPage = () => {
                 {cities.map((city) => (
                   <div
                     key={city.id}
-                    onClick={() => handleCityChange(city.name)}
+                    onClick={() => handleCityChange(city)}
                     className="px-4 py-2 cursor-pointer hover:bg-gray-800 hover:text-white"
                   >
                     {city.name}
@@ -108,10 +189,10 @@ const SearchPage = () => {
         </div>
         <div className="sort-by flex items-center space-x-3 p-3" style={{ flexBasis: '30%' }}>
           <WWButton variant="v1" minWidth="8rem" text="Sort By" />
-          <WWButton variant="v3" minWidth="6rem" text="Distance" className="rounded-full" />
-          <WWButton variant="v3" minWidth="6rem" text="Time" className="rounded-full" />
-          <WWButton variant="v3" minWidth="6rem" text="Price" className="rounded-full" />
-          <WWButton variant="v3" minWidth="6rem" text="Rating" className="rounded-full" />
+          <WWButton variant="v3" minWidth="6rem" text="Distance" className="rounded-full" onClick={sortByDistance} />
+          <WWButton variant="v3" minWidth="6rem" text="Time" className="rounded-full" onClick={sortByTime} />
+          <WWButton variant="v3" minWidth="6rem" text="Price" className="rounded-full" onClick={sortByPrice} />
+          <WWButton variant="v3" minWidth="6rem" text="Rating" className="rounded-full" onClick={sortByRating} />
           {/* <button className="bg-red-600  text-white py-2 px-2 rounded-md min-w-[10rem]">Sort By</button>
           <button className="bg-gray-900 text-white py-2 px-2 rounded-md min-w-[10rem]">Distance</button>
           <button className="bg-gray-900 text-white py-2 px-2 rounded-md min-w-[10rem]">Time</button>
@@ -126,6 +207,7 @@ const SearchPage = () => {
             gymName={gym.gym_name}
             imageSrc={gym.images[0]}
             rating={gym.average_rating}
+            gymId={gym._id}
           />
         ))}
       </div>
