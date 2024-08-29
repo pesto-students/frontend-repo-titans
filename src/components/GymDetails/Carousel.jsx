585784278c5ba94309ca034ae4e54563259a782a
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-08-29 19:03:38 +0530

    fix: carousel thumbnail images aligned properly

--- src/components/GymDetails/Carousel.jsx
+++ src/components/GymDetails/Carousel.jsx
@@ -1,11 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react'
-import Slider from 'react-slick'
-import PropTypes from 'prop-types'
+import React, { useRef, useState, useEffect } from "react";
+import Slider from "react-slick";
+import PropTypes from "prop-types";
 
 function Carousel({ images }) {
-  const sliderRef = useRef(null)
-
-
+  const sliderRef = useRef(null);
 
   const settings = {
     dots: true,
@@ -15,22 +13,22 @@ function Carousel({ images }) {
     slidesToScroll: 1,
     arrows: true,
     fade: true,
-  }
+  };
 
   const handleThumbnailClick = (index) => {
     if (sliderRef.current) {
-      sliderRef.current.slickGoTo(index)
+      sliderRef.current.slickGoTo(index);
     }
-  }
+  };
 
   return (
-    <div className='p-4 md:px-8'>
+    <div className="p-4 md:px-8">
       <Slider ref={sliderRef} {...settings}>
         {images.map((image, index) => (
-          <div key={index} className='h-56 md:h-96'>
+          <div key={index} className="h-56 md:h-96">
             <img
               src={image}
-              className='object-cover w-full h-full'
+              className="object-cover w-full h-full"
               alt={`carousel-${index}`}
             />
           </div>
@@ -38,27 +36,27 @@ function Carousel({ images }) {
       </Slider>
 
       {/* Thumbnail Navigation */}
-      <div className='justify-between hidden mt-4 space-x-4 overflow-x-auto sm:flex'>
+      <div className="justify-start hidden mt-4 space-x-4 overflow-x-auto sm:flex">
         {images.slice(0, 4).map((image, index) => (
           <div
             key={index}
-            className='w-1/5 cursor-pointer'
+            className="w-1/5 cursor-pointer"
             onClick={() => handleThumbnailClick(index)}
           >
             <img
               src={image}
-              className='object-cover w-full h-full'
+              className="object-cover w-full h-full"
               alt={`thumbnail-${index}`}
             />
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 Carousel.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+};
 
-export default Carousel
+export default Carousel;
