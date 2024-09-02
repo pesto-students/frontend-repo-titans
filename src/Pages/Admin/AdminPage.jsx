658028abe7714905d4255f7af472f49479855694
Dev Saini <devwithsmile007@gmail.com>
2024-09-02 17:14:15 +0530

    Update AdminPage.jsx

--- src/Pages/Admin/AdminPage.jsx
+++ src/Pages/Admin/AdminPage.jsx
@@ -5,85 +5,102 @@ import Carousel from '../../components/GymDetails/Carousel';
 import { FaArrowLeft, FaArrowRight, FaCheck, FaTimes } from 'react-icons/fa';
 import moment from 'moment';
 
+
 const GymDetails = ({ gym, handlePrev, handleNext, handleApprove, handleDeny, page, totalPages }) => {
     return (
-        <div className="flex flex-col p-6 text-gray-200 bg-black border rounded-lg shadow-md gym-details lg:flex-row">
-            {gym.images && gym.images.length > 0 && (
-                <div className="mb-4 lg:mb-0 lg:mr-6 lg:w-96">
-                    <Carousel images={gym.images} />
-                </div>
-            )}
+        <div className="flex flex-col p-8 bg-transparent rounded-lg shadow-lg gym-details lg:flex-row lg:space-x-6">
+            <div className="mb-6 lg:w-1/3 lg:mb-0">
+                {gym.images && gym.images.length > 0 && (
+                    <div className="overflow-hidden rounded-lg shadow-md">
+                        <Carousel images={gym.images} />
+                    </div>
+                )}
+            </div>
 
             <div className="flex-1">
-                <div className="flex justify-between">
-                    <h2 className="mb-4 text-3xl font-bold text-gray-100">{gym.gym_name}</h2>
-                    <span className="self-center hidden text-sm text-gray-500 md:!block">
-                        {page}/{totalPages}
+                <div className="flex items-center justify-between mb-4">
+                    <h2 className="text-4xl font-extrabold text-white">{gym.gym_name}</h2>
+                    <span className="text-sm text-gray-400">
+                        Page {page} of {totalPages}
                     </span>
                 </div>
 
-                <p className="mb-4">
-                    <strong className="text-gray-300">Description:</strong>
-                    <span className="text-gray-400"> {gym.description}</span>
+                <p className="mb-4 text-gray-300">
+                    <strong>Description:</strong> <span className="text-gray-200">{gym.description}</span>
                 </p>
                 <hr className="my-6 border-gray-600" />
 
-                <div className="flex flex-col my-4 lg:flex-row">
-                    <div className="mb-4 lg:w-1/2 lg:mb-0">
+                <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+                    <div>
                         <h4 className="font-semibold text-gray-300">Address:</h4>
-                        <p className="text-gray-400">{gym.address.address_line_1}</p>
-                        <p className="text-gray-400">{gym.address.address_line_2}</p>
-                        <p className="text-gray-400">{gym.address.city}, {gym.address.state} - {gym.address.pincode}</p>
+                        <p className="text-gray-200">{gym.address.address_line_1}</p>
+                        <p className="text-gray-200">{gym.address.address_line_2}</p>
+                        <p className="text-gray-200">
+                            {gym.address.city}, {gym.address.state} - {gym.address.pincode}
+                        </p>
                     </div>
-                    <div className="lg:w-1/2">
+
+                    <div>
                         <h4 className="font-semibold text-gray-300">Coordinates:</h4>
-                        <p className="text-gray-400">Latitude: {gym.map_detail.coordinates[1]}</p>
-                        <p className="text-gray-400">Longitude: {gym.map_detail.coordinates[0]}</p>
+                        <p className="text-gray-200">Latitude: {gym.map_detail.coordinates[1]}</p>
+                        <p className="text-gray-200">Longitude: {gym.map_detail.coordinates[0]}</p>
                     </div>
                 </div>
                 <hr className="my-6 border-gray-600" />
 
-                <div className="flex flex-col md:flex-row">
-                    <div className="mb-4 md:mb-0">
-                        <p><strong className="text-gray-300">GST:</strong> <span className="text-gray-400">{gym.gst_number}</span></p>
-                        <p><strong className="text-gray-300">Occupancy:</strong> <span className="text-gray-400">{gym.total_occupancy}</span></p>
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                    <div>
+                        <p><strong className="text-gray-300">GST:</strong> <span className="text-gray-200">{gym.gst_number}</span></p>
+                        <p><strong className="text-gray-300">Occupancy:</strong> <span className="text-gray-200">{gym.total_occupancy}</span></p>
                     </div>
-                    <hr className="my-6 md:hidden" />
-                    <div className="md:ml-48">
-                        <p><strong className="text-gray-300">Status:</strong> <span className="text-gray-400">{gym.status}</span></p>
-                        <p><strong className="text-gray-300">Request Date:</strong> <span className="text-gray-400">{moment(gym.req_creation_Date).format('D MMMM YYYY')}</span></p>
+                    <div>
+                        <p><strong className="text-gray-300">Status:</strong> <span className="text-gray-200">{gym.status}</span></p>
+                        <p><strong className="text-gray-300">Request Date:</strong> <span className="text-gray-200">{moment(gym.req_creation_Date).format('D MMMM YYYY')}</span></p>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-600" />
-                <div className='flex flex-col gap-10 md:gap-24 md:flex-row '>
-                    <div>
-                        <p><strong className="text-gray-300">Email:</strong> <span className="text-gray-400">{gym.owner_id.email}</span></p>
-                        <p><strong className="text-gray-300">Phone Number:</strong> <span className="text-gray-400">{gym.owner_id.phone_number}</span></p>
-                    </div>
-                    <div>
-                        <p><strong className="text-gray-300">Upi:</strong> <span className="text-gray-400">{gym.owner_id.upi_id}</span></p>
 
+                <hr className="my-6 border-gray-600" />
+
+                <div className="flex flex-col gap-6 md:flex-row">
+                    <div className="flex-1">
+                        <p><strong className="text-gray-300">Email:</strong> <span className="text-gray-200">{gym.owner_id.email}</span></p>
+                        <p><strong className="text-gray-300">Phone Number:</strong> <span className="text-gray-200">{gym.owner_id.phone_number}</span></p>
+                    </div>
+                    <div className="flex-1">
+                        <p><strong className="text-gray-300">UPI:</strong> <span className="text-gray-200">{gym.owner_id.upi_id}</span></p>
                     </div>
                 </div>
+
                 <hr className="my-6 border-gray-600" />
+
+                {/* Reason Textarea */}
+                <div className="mb-6">
+                    <label className="block mb-2 text-gray-300" htmlFor="reason">Reason:</label>
+                    <textarea
+                        id="reason"
+                        rows="4"
+                        className="w-full p-3 text-gray-900 border "
+                        placeholder="Enter your reason here..."
+                    />
+                </div>
 
                 <div className="flex flex-col items-center justify-between mt-6 space-y-4 lg:flex-row lg:space-y-0">
                     {/* Left side: Prev and Next buttons */}
                     <div className="flex justify-start space-x-4 lg:w-1/2">
-                        <button onClick={handlePrev} className="flex items-center px-4 py-2 text-white bg-blue-500 rounded">
+                        <button onClick={handlePrev} className="flex items-center px-4 py-2 text-white transition bg-blue-600 rounded-lg hover:bg-blue-500">
                             <FaArrowLeft className="mr-2" /> Prev
                         </button>
-                        <button onClick={handleNext} className="flex items-center px-4 py-2 text-white bg-blue-500 rounded">
+                        <button onClick={handleNext} className="flex items-center px-4 py-2 text-white transition bg-blue-600 rounded-lg hover:bg-blue-500">
                             Next <FaArrowRight className="ml-2" />
                         </button>
                     </div>
 
                     {/* Right side: Approve and Deny buttons */}
                     <div className="flex justify-end space-x-4 lg:w-1/2">
-                        <button onClick={handleApprove} className="flex items-center px-4 py-2 text-white bg-green-500 rounded">
+                        <button onClick={handleApprove} className="flex items-center px-4 py-2 text-white transition bg-green-600 rounded-lg hover:bg-green-500">
                             <FaCheck className="mr-2" /> Approve
                         </button>
-                        <button onClick={handleDeny} className="flex items-center px-4 py-2 text-white bg-red-500 rounded">
+                        <button onClick={handleDeny} className="flex items-center px-4 py-2 text-white transition bg-red-600 rounded-lg hover:bg-red-500">
                             <FaTimes className="mr-2" /> Deny
                         </button>
                     </div>
@@ -93,10 +110,11 @@ const GymDetails = ({ gym, handlePrev, handleNext, handleApprove, handleDeny, pa
     );
 };
 
+
 function AdminPage() {
     const [gym, setGym] = useState(null);
     const [pageNo, setPageNo] = useState(1);
-    const [totalPages, setTotalPages] = useState(1); // Assuming you get the total number of pages from the API
+    const [totalPages, setTotalPages] = useState(1); 
 
     const fetchGymData = async (page) => {
         try {
